b48d8ab0836f3e18392495507933d535fc7be42e
Leonardo Giovannoni <leogiovannoni@gmail.com>
2024-02-17 20:03:03 +0100

    added rc.rs, currently don't used

---
+++ refslice/src/rc.rs
@@ -0,0 +1,63 @@
+// custom implementation of rc without weak references
+
+use std::{cell::Cell, ops::Deref, ptr::NonNull};
+
+#[repr(C)]
+struct RcBox<T: ?Sized> {
+    ref_count: Cell<usize>,
+    value: T,
+}
+
+pub struct Rc<T: ?Sized> {
+    ptr: NonNull<RcBox<T>>,
+}
+
+impl<T> Rc<T> {
+    pub fn new(value: T) -> Self {
+        let ptr = Box::into_raw(Box::new(RcBox {
+            ref_count: Cell::new(1),
+            value,
+        }));
+
+        // SAFETY: Box::into_raw returns a non-null pointer
+        Self {
+            ptr: unsafe { NonNull::new_unchecked(ptr) },
+        }
+    }
+}
+
+impl<T: ?Sized> Clone for Rc<T> {
+    fn clone(&self) -> Self {
+         // SAFETY: self.ptr is a valid pointer
+         unsafe {
+            (*self.ptr.as_ptr()).ref_count.set((*self.ptr.as_ptr()).ref_count.get() + 1);
+        }
+        Self { ptr: self.ptr }
+    }
+}
+
+impl<T: ?Sized> Drop for Rc<T> {
+    fn drop(&mut self) {
+
+        unsafe {
+            // SAFETY if the ref_count is 1, then we are the last reference to this data
+            if (*self.ptr.as_ptr()).ref_count.get() == 1 {
+                // so we need to deallocate the data
+                let b = Box::from_raw(self.ptr.as_ptr());
+                drop(b);
+            } else {
+                // otherwise, we need to decrement the ref_count
+                (*self.ptr.as_ptr()).ref_count.set((*self.ptr.as_ptr()).ref_count.get() - 1);
+            }
+        }
+    }
+}
+
+impl<T: ?Sized> Deref for Rc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        // SAFETY: self.ptr is a valid pointer
+        unsafe { &self.ptr.as_ref().value }
+    }
+}
