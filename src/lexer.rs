f80d28c6c3350ea32af52f91f871ee0b79174e17
Leonardo Giovannoni <leogiovannoni@gmail.com>
2024-07-20 22:09:41 +0200

    make lexer more idiomatic

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,77 +1,93 @@
 use crate::util::Span;
 use crate::*;
+use itertools::{peek_nth, PeekNth};
+use std::str::CharIndices;
 
 pub struct Lexer<'a> {
-    pub pos: usize,
+    chars: PeekNth<CharIndices<'a>>,
     pub text: &'a str,
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
         Lexer {
-            pos: 0,
+            chars: peek_nth(input.char_indices()),
             text: input,
         }
     }
 
-    pub fn token_and_advance(&mut self, kind: TokenKind, token_len: usize) -> Token {
-        let token = Token::new(kind, Span::new(self.pos, self.pos + token_len));
-        self.pos += token_len;
-        token
+    fn match_identifier(&mut self, start: usize) -> (&'a str, usize) {
+        let end;
+        loop {
+            if let Some((i, ch)) = self.chars.peek() {
+                if ch.is_alphabetic() {
+                    self.chars.next();
+                } else {
+                    end = *i;
+                    break;
+                }
+            } else {
+                end = self.text.len();
+                break;
+            }
+        }
+        (&self.text[start..end], end)
     }
 
-    fn count_bytes_until(&self, f: impl Fn(char) -> bool) -> usize {
-        self.text[self.pos..]
-            .chars()
-            .take_while(|x| f(*x))
-            .map(|x| x.len_utf8())
-            .sum()
-    }
-
-    fn skip_while(&mut self, f: impl Fn(char) -> bool) {
-        self.pos += self.count_bytes_until(f);
-    }
-
-    fn peek(&self) -> Option<char> {
-        self.text[self.pos..].chars().next()
-    }
-
-    fn peek_str(&self, len: usize) -> Option<&str> {
-        self.text.get(self.pos..self.pos + len)
+    fn match_number(&mut self, start: usize) -> (&'a str, usize) {
+        let end;
+        loop {
+            if let Some((i, ch)) = self.chars.peek() {
+                if ch.is_ascii_digit() {
+                    self.chars.next();
+                } else {
+                    end = *i;
+                    break;
+                }
+            } else {
+                end = self.text.len();
+                break;
+            }
+        }
+        (&self.text[start..end], end)
     }
 
     pub fn next(&mut self) -> Token {
-        self.skip_while(char::is_whitespace);
-        let Some(c) = self.peek() else {
-            return Token::new(TokenKind::Eoi, Span::empty());
-        };
-        match c {
-            x if x.is_alphabetic() => {
-                let i = self.count_bytes_until(char::is_alphabetic);
-                let kind = match self.peek_str(i) {
-                    Some("with") => TokenKind::KWWith,
-                    _ => TokenKind::Ident,
-                };
-                self.token_and_advance(kind, i)
-            }
-            x if x.is_ascii_digit() => {
-                let i = self.count_bytes_until(|x| x.is_ascii_digit());
-                self.token_and_advance(TokenKind::Number, i)
-            }
-            _ => self.token_and_advance(
-                match c {
-                    '+' => TokenKind::Plus,
-                    '-' => TokenKind::Minus,
-                    '*' => TokenKind::Star,
-                    '/' => TokenKind::Slash,
-                    '(' => TokenKind::LParen,
-                    ')' => TokenKind::RParen,
-                    ':' => TokenKind::Colon,
-                    ',' => TokenKind::Comma,
-                    _ => TokenKind::Unknown,
-                },
-                c.len_utf8(),
-            ),
+        loop {
+            match self.chars.next() {
+                Some((_, c)) if c.is_whitespace() => {
+                    continue;
+                }
+                Some((i, c)) if c.is_alphabetic() => {
+                    let (ident, end) = self.match_identifier(i);
+                    break match ident {
+                        "with" => Token::new(TokenKind::KWWith, Span::new(i, end)),
+                        _ => Token::new(TokenKind::Ident, Span::new(i, end)),
+                    };
+                }
+                Some((i, c)) if c.is_ascii_digit() => {
+                    let (_, end) = self.match_number(i);
+                    break Token::new(TokenKind::Number, Span::new(i, end));
+                }
+                Some((i, c)) => {
+                    break Token::new(
+                        match c {
+                            '+' => TokenKind::Plus,
+                            '-' => TokenKind::Minus,
+                            '*' => TokenKind::Star,
+                            '/' => TokenKind::Slash,
+                            '(' => TokenKind::LParen,
+                            ')' => TokenKind::RParen,
+                            ':' => TokenKind::Colon,
+                            ',' => TokenKind::Comma,
+                            _ => TokenKind::Unknown,
+                        },
+                        Span::new(i, i + c.len_utf8()),
+                    );
+                }
+
+                None => break Token::new(TokenKind::Eoi, Span::empty()),
+            };
         }
     }
 }
